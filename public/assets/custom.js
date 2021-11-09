e1fcbabd3110272a569e291e99898ff85e45ad7e
Sharan <sharan@mallow-tech.com>
2021-11-09 22:37:06 +0530

    - Added smooth scroll. - Fixed heading scroll.

--- public/assets/custom.js
+++ public/assets/custom.js
@@ -19,5 +19,25 @@ $(function () {
         if (option.data('page') !== 'current') {
             window.location.href = option.data('url');
         }
-    })
+    });
+
+    // Animate Scroll to Heading
+    function loadHash(hashObj) {
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $("html, body").animate({scrollTop: $(hashObj).offset().top - 70}, 500);
+    }
+
+    // Scroll to Heading
+    $('#markdown_content > h1 + ul > li a').on('click', function (event) {
+        if (this.hash !== "") {
+            let hash = this.hash;
+            // Prevent default anchor click behavior
+            event.preventDefault();
+            loadHash(hash);
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        }
+    });
+    if (window.location.hash) loadHash(window.location.hash);
 });
