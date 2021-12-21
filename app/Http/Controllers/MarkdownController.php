8e272a68a3f4fed2688750a3f923bbaeba3355d9
Sharan <sharan@mallow-tech.com>
2021-12-21 13:37:28 +0530

    - Updated refresh markdown controller.

--- app/Http/Controllers/MarkdownController.php
+++ app/Http/Controllers/MarkdownController.php
@@ -2,21 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Cache\Repository as Cache;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Symfony\Component\Process\Process;
 
 class MarkdownController extends Controller
 {
     /**
+     * Cache Repository
+     *
+     * @var \Illuminate\Contracts\Foundation\Application|mixed
+     */
+    protected $cache;
+
+    public function __construct()
+    {
+        $this->cache = app(Cache::class);
+    }
+
+    /**
      * Update Markdown using compose Command
      */
-    public function refresh()
+    public function refresh(Request $request)
     {
-        $passkey = request()->get('passkey');
+        $passkey = $request->get('passkey');
         if (isset($passkey)) {
             if (Hash::check($passkey, env('MARKDOWN_REFRESH_PASSKEY'))) {
                 shell_exec('cd ' . base_path() . ' && markdown/get.sh');
+                $this->cache->flush();
 
                 return 'Executed Successful';
             }
