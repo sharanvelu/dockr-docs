7dbac0e2bb6588ccdb2bf205b2eb61896d95e7d7
Sharan <sharan@mallow-tech.com>
2021-12-22 10:13:34 +0530

    - Updated links to open repo in new tab

--- resources/views/docs/sidebar.blade.php
+++ resources/views/docs/sidebar.blade.php
@@ -3,10 +3,10 @@
 
     <div class="sidebar-links p-2">
         <div class="row justify-content-around m-0">
-            <a class="repo-link-icon" href="{{ $dockrGithub }}">
+            <a class="repo-link-icon" href="{{ $dockrGithub }}" target="_blank">
                 <img src="{{ $githubLogo }}" alt="Github Logo">
             </a>
-            <a class="repo-link-icon" href="{{ $dockrDockerHub }}">
+            <a class="repo-link-icon" href="{{ $dockrDockerHub }}" target="_blank">
                 <img class="docker-hub-logo" src="{{ $dockerHubLogo }}" alt="Docker Hub Logo">
             </a>
         </div>
