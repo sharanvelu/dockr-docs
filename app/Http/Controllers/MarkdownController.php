b67f0e92010feaefd28dd447fddf879dc8e1a269
Sharan <sharan@mallow-tech.com>
2022-01-02 19:32:39 +0530

    - Removed markdown refresh flow(as it is not working).

--- app/Http/Controllers/MarkdownController.php
+++ app/Http/Controllers/MarkdownController.php
@@ -8,22 +8,5 @@ use Illuminate\Support\Facades\Hash;
 
 class MarkdownController extends Controller
 {
-    /**
-     * Update Markdown using compose Command
-     * @throws \Exception
-     */
-    public function refresh(Request $request)
-    {
-        $passkey = $request->get('passkey');
-        if (isset($passkey)) {
-            if (Hash::check($passkey, env('MARKDOWN_REFRESH_PASSKEY'))) {
-                info(shell_exec('cd ' . base_path() . ' && sudo markdown/get.sh'));
-                cache()->flush();
-
-                return 'Executed Successful';
-            }
-        }
-
-        return 'Passkey incorrect';
-    }
+    //
 }
