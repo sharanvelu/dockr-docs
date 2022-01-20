e4c6d272620cc2d193603bba5313863d53115f2a
Sharan <sharan@mallow-tech.com>
2022-01-20 16:01:39 +0530

    - Updated slack webhook data

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class WebhookController extends Controller
 {
     protected function dockerHub(Request $request)
     {
-        info('request', $request->all());
+        $response = json_decode(json_encode($request->all()), -1);
+        $repo = $response['repository'];
+
+        $data = [
+            'image' => [
+                'images' => $response['push_data']['images'],
+                'pushed_at' => Carbon::parse($response['push_data']['pushed_at'])->format('d-m-Y'),
+            ],
+            'repo' => [
+                'name' => $repo['name'],
+                'namespace' => $repo['name'],
+                'owner' => $repo['owner'],
+                'repo_name' => $repo['repo_name'],
+                'repo_url' => $repo['repo_url'],
+                'star_count' => $repo['star_count'],
+                'status' => $repo['status'],
+            ]
+        ];
+
+        Log::channel('docker_hub')->info('Docker Hub', $data);
+
+        return response()->json([], 200);
     }
 }
